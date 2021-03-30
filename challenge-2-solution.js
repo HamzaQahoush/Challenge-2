346d58fe35e6378b2c73562aece888edc4af4180
Hamza Qahoush <haamza.qah@gmail.com>
2021-03-30 13:34:56 +0300

    challange 2

---
+++ challenge-2-solution.js
@@ -0,0 +1,26 @@
+'use  strict';
+
+let firstNo= prompt('Enter the first No');
+let SecondNo= prompt('Enter the second No');
+let third= prompt('Enter the Third No');
+let fourth= prompt('Enter the Fourth No');
+let fivth= prompt('Enter the Fith No');
+
+
+function max(){
+
+return alert(('This is the Max ')+ (Math.max(firstNo,SecondNo,third,fourth,fivth)))
+
+}
+
+
+
+
+function min(){
+
+    return alert (('This is the Min ')+ (Math.min(firstNo,SecondNo,third,fourth,fivth)))
+    
+    }
+    
+    min()
+    max()
